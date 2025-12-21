5051a77de3d9091542efd365dd6cf686a197b71a
naorex <nao23r@gmail.com>
2025-12-21 16:54:37 +0900

    UPDATE : Result.js

--- react-corona-tracker-app/src/App.js
+++ react-corona-tracker-app/src/App.js
@@ -1,10 +1,37 @@
-import TopPage from './pages/TopPages';
+import { useState } from 'react';
+import countriesJson from './countries.json';
+import TopPage from './pages/TopPage';
 import './App.css';
 
 function App() {
+  // 取得した slug を保存する変数
+  const [country, setCountry] = useState('');
+
+  const [countryData, setCountryData] = useState({
+    date: '',
+    newConfirmed: '',
+    totalConfirmed: '',
+    newRecovered: '',
+    totalRecovered: '',
+  });
+
+  const getCountryData = () => {
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountryData({
+          date: data[data.length - 1].Date,
+          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+          totalConfirmed: data[data.length - 1].Confirmed,
+          newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+          totalRecovered: data[data.length - 1].Recovered,
+        });
+      });
+  };
   return (
     <div>
-      <TopPage></TopPage>
+      {console.log(countryData)}
+      <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}></TopPage>
     </div>
   );
 }
---
+++ react-corona-tracker-app/src/pages/TopPage.js
@@ -0,0 +1,15 @@
+import Title from '../components/Title';
+import Selector from '../components/Selector';
+import Results from '../components/Results';
+
+const TopPage = (props) => {
+  return (
+    <div>
+      <Title></Title>
+      <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData}></Selector>
+      <Results></Results>
+    </div>
+  );
+};
+
+export default TopPage;
