0d0693566504f353de519e6dcef565e4f0247867
naorex <nao23r@gmail.com>
2025-12-21 15:59:24 +0900

    UPDATE : Selector.js -> create getCountryData()

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # はじめてさわるReact & JavaScript with TypeScript
 
 [参考（著者HP）](https://github.com/mod728/react-book--corona-tracker-typescript)
+
+## dev サーバー起動
+
+- react-corona-tracker-app フォルダ直下で
+
+```shell
+npm start
+```
--- react-corona-tracker-app/src/components/Selector.js
+++ react-corona-tracker-app/src/components/Selector.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import countriesJson from '../countries.json';
 
 const Selector = () => {
+  // 取得した slug を保存する変数
+  const [country, setCountry] = useState('');
+
+  const getCountryData = () => {
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div>
-      <select>
+      <select onChange={(e) => setCountry(e.target.value)}>
+        <option>Select A Country</option>
         {countriesJson.map((country, index) => (
-          <option key={index}>{country.Country}</option>
+          <option key={index} value={country.Slug}>
+            {country.Country}
+          </option>
         ))}
       </select>
+      <button onClick={getCountryData}>Get Data</button>
     </div>
   );
 };
